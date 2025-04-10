e20917367490dabaa634b29abc1167ab5375213d
Abhishek Kumar <57810907+abhishek-dataengineer@users.noreply.github.com>
2025-04-10 12:45:20 +0530

    Update DataBase.py

--- OnetimeConfigFiles/DataBase.py
+++ OnetimeConfigFiles/DataBase.py
@@ -1,18 +1,18 @@
 # Databricks notebook source
 # MAGIC %sql
-# MAGIC create database if not exists silver;
+create database if not exists silver;
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC create database if not exists bronze;
+create database if not exists bronze;
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC create database if not exists gold;
+ database if not exists gold;
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC create database if not exists gold_vw;
+ create database if not exists gold_vw;
